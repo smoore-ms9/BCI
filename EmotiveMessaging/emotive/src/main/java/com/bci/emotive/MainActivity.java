83ac19ee342b6fb9b6da30871a3452d9278c6b4c
Stephen Moore <smoore.ms9@gmail.com>
2014-01-01 10:26:32 -0500

    Google GCM Integration

--- EmotiveMessaging/emotive/src/main/java/com/bci/emotive/MainActivity.java
+++ EmotiveMessaging/emotive/src/main/java/com/bci/emotive/MainActivity.java
@@ -1,7 +1,6 @@
 package com.bci.emotive;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,7 +8,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +17,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Testing GCM library
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
 
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
